8f15d41f1c34ba9ca8c3b635c1589522b3345b6a
Jonathan Salomon <jonpaulsalomon@gmail.com>
2020-02-08 17:50:06 -0300

    asdasdasd

--- cs20483/cs20486/CS20486.Tipos/UsingTypes.cs
+++ cs20483/cs20486/CS20486.Tipos/UsingTypes.cs
@@ -13,15 +13,21 @@ namespace CS20486.Tipos
             Console.WriteLine("Conversão de tipos valor");
             byte n1 = 10;
             short n2 = 10;
-            long n3 = 20;
+            long n3 = 2000000000000000000;
             short n4 = 30;
             //var - tipo definido durante a atribuição
             var result = n1 + n2;
             var res2 = n3 + n4;
 
-            // object é o tipo mais básico do c#
+            // object é o tipo mais básico do c#, pode converter tudo para object
             object res3 = n3 + n1;
+
+            // conversão explicita / cast
+            var conversaoExplicita = (int)n3;
+            Console.WriteLine($"conversaoExplicita: {conversaoExplicita}");
             
+            var conversaoComConvert = Convert.ToInt32(n4);
+
             Console.WriteLine($"A soma é { result } - e resulta em um '{result.GetType()}'");
             Console.WriteLine($"A soma 2 é { res2 } - e resulta em um '{res2.GetType()}'");
             Console.WriteLine($"A soma 3 é { res3 } - e resulta em um '{res3.GetType()}'");

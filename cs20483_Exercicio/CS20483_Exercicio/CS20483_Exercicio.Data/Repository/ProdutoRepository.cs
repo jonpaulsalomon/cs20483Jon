b2e1fec655f5f861d9dc7317c28e9960531c909a
Jonathan Salomon <jonpaulsalomon@gmail.com>
2020-02-22 19:16:00 -0300

    ProdutoRepository

--- cs20483_Exercicio/CS20483_Exercicio/CS20483_Exercicio.Data/Repository/ProdutoRepository.cs
+++ cs20483_Exercicio/CS20483_Exercicio/CS20483_Exercicio.Data/Repository/ProdutoRepository.cs
@@ -2,6 +2,7 @@
 using CS20483_Exercicio.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,32 +13,56 @@ namespace CS20483_Exercicio.Data.Repository
     {
         public List<Produto> ListAll()
         {
-            Cs20483Context context = new Cs20483Context();
-            List<Produto> list = context.Produto.ToList();
-            context.Dispose();
+            List<Produto> list = new List<Produto>();
+            using (Cs20483Context context = new Cs20483Context())
+            {
+                list = context.Produto.ToList();
+            }
+            
             return list;
         }
 
         public Produto GetById(int id)
         {
             var model = new Produto();
+            using (Cs20483Context context = new Cs20483Context())
+            {
+                model = context.Produto.Find(id);
+                //model = context.Produto.FirstOrDefault(p => p.Id == id);
+            }
 
             return model;
         }
 
         public string Create(Produto model)
         {
+            using (Cs20483Context context = new Cs20483Context())
+            {
+                context.Produto.Add(model);
+                context.SaveChanges();
+            }
 
             return "Inserido com sucesso";
         }
 
         public string Update(Produto model)
         {
+            using (Cs20483Context context = new Cs20483Context())
+            {
+                context.Entry<Produto>(model).State = EntityState.Modified;
+                context.SaveChanges();
+            }
             return "Alterado com sucesso";
         }
 
         public string Delete(int id)
         {
+            using (Cs20483Context context = new Cs20483Context())
+            {
+                context.Produto.Remove(GetById(id));
+                context.SaveChanges();
+            }
+
             return "Deletado com sucesso";
         }
     }

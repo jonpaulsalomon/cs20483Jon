cca03eacc9863ca4928544fe381a7bf284e8fb18
Jonathan Salomon <jonpaulsalomon@gmail.com>
2020-02-22 15:45:56 -0300

    ef

---
+++ cs20483_Exercicio/CS20483_Exercicio/CS20483_Exercicio.Data/Context/CS20483Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS20483_Exercicio.Data.Context
+{
+    public class Cs20483Context
+    {
+
+    }
+}

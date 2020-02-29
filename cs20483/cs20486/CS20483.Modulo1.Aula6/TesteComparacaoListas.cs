918f2a4eef071fd774e5c2da410975b16ad7b107
Jonathan Salomon <jonpaulsalomon@gmail.com>
2020-02-29 20:16:43 -0300

    aula

--- cs20483/cs20486/CS20483.Modulo1.Aula6/TesteComparacaoListas.cs
+++ cs20483/cs20486/CS20483.Modulo1.Aula6/TesteComparacaoListas.cs
@@ -11,21 +11,22 @@ namespace CS20483.Modulo1.Aula6
         public void Executa()
         {
             List<PessoaJuridica> listaPJ = new List<PessoaJuridica>();
-            PessoaJuridica pj1 = new PessoaJuridica { Cnpj = "50", Nome = "a", Nascimento = new DateTime(1950, 05, 2) };
-            PessoaJuridica pj2 = new PessoaJuridica { Cnpj = "40", Nome = "b", Nascimento = new DateTime(1975, 05, 2) };
-            PessoaJuridica pj3 = new PessoaJuridica { Cnpj = "30", Nome = "c", Nascimento = new DateTime(1970, 05, 2) };
+            PessoaJuridica pj1 = new PessoaJuridica { Cnpj = "50", Nome = "Empresa1", Nascimento = new DateTime(1950, 05, 2) };
+            PessoaJuridica pj2 = new PessoaJuridica { Cnpj = "40", Nome = "Empresa2", Nascimento = new DateTime(1975, 05, 2) };
+            PessoaJuridica pj3 = new PessoaJuridica { Cnpj = "30", Nome = "Empresa3", Nascimento = new DateTime(1970, 05, 2) };
 
             listaPJ.Add(pj1);
             listaPJ.Add(pj2);
             listaPJ.Add(pj3);
 
             listaPJ.Sort();
-            listaPJ.OrderByDescending(pj => pj.Cnpj);
 
-            foreach (var item in listaPJ)
-            {
-                Console.WriteLine(item);
-            }        
+            //foreach (var item in listaPJ)
+            //{
+            //    Console.WriteLine(item);
+            //}        
+
+            Console.WriteLine(listaPJ.FirstOrDefault());
         }
     }
 }
